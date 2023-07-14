e86679a84fe7c0a75119e0251e85e4dbf00b6aaa
ir0nc0w <cwmyung@snu.ac.kr>
2023-07-14 15:20:35 +0900

    init: forum

---
+++ forum/app/page.js
@@ -0,0 +1,15 @@
+import { connectDB } from '@/util/database'
+
+export default async function Home() {
+
+  const client = await connectDB
+  const db = client.db("forum")
+  let result = await db.collection('post').find().toArray()
+  console.log(result)
+
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
